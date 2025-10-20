33611d8de64577eb0c2df1d3d5ce9e534917b934
sam31124 <sa.duocuc,cl}git config --global user.name sam31124git config --global user.email sa.duocuc,cl>
2025-10-20 10:00:00 +0000

    Commit 1: Estructura base del proyecto (MVVM + paquetes iniciales)

---
+++ app/src/main/java/com/libreriag/app/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.libreriag.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.libreriag.app.ui.home.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/libreriag/app/ui/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.libreriag.app.ui.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("LibreríaG - Pantalla  Principal")
+}
